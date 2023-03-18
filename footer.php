2e0605090dbabcc9f88d6f77f9d77bee5f5af6fd
safoora <safoura.mansouri@gmail.com>
2023-03-18 02:51:16 -0400

    C14 ajouter menu au pied

--- footer.php
+++ footer.php
@@ -3,7 +3,10 @@
 ?>
 <footer class="site__footer">
     <h3>tous les droits sont réservés</h3>
-
+    <?php wp_nav_menu(array(
+                    'menu'  => 'footer',
+                    'container' => 'nav'
+                )); ?> 
 </footer>
 <?php wp_footer(); ?>
 </body>
