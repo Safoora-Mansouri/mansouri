1afab5b4b2810080c0c8b8ac274f42e492f13053
safoora <safoura.mansouri@gmail.com>
2023-04-09 12:11:30 -0400

    C19 ameliorer le style pour format moble

--- template-parts/categorie-cours.php
+++ template-parts/categorie-cours.php
@@ -11,7 +11,7 @@ $duree = "90h";
 ?>
 <article class="blocflex__article">
     <h5><a href="<?php the_permalink(); ?>"> <?= $sigle; ?></a></h5>
-    <h6><?= $titre_long ?></h6>
+  
     <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
     <p><?php the_field('enseignant');?></p>
     <p><?= $duree ?></p>
